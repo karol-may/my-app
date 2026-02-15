fe4af3e21dbe07f8502e97b998c1ecdb90dd5ebb
Karol May <karol@imay.pl>
2026-02-15 12:41:11 +0100

    qr added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import env from 'react-dotenv';
+import { QRCodeSVG } from 'qrcode.react';
 import './App.css'
 
+const APP_URL = env?.APP_URL ?? "http://localhost:5173";
+
+function LoginForm({buttonOnClickHandler}){
+  return(
+    <form
+      style={{display: 'flex', flexDirection: 'column'}}
+      onSubmit={(e)=>{
+        e.preventDefault();
+        buttonOnClickHandler(e);
+      }}
+      >
+      <label htmlFor="email">e-mail</label>
+      <input type="text" id="email"/>
+      <label htmlFor="password">hasło</label>      
+      <input type="password" id="password"/>
+      <hr/>
+      <button>Zaloguj</button>
+    </form>
+  )
+}
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+
+  function buttonOnClickHandler(e){
+    setUser("karol@imay.pl");
+  }
 
   return (
     <>
-      Hi, All let's get know each other!
+      { user &&
+          <>
+            Hi {user}, All let's get know each other!
+            <QRCodeSVG value={APP_URL+"/"+user}></QRCodeSVG>
+          </>
+      }
+
+      { !user &&
+        <>
+          <LoginForm buttonOnClickHandler={buttonOnClickHandler}/>        
+        </>
+      }     
     </>
   )
 }
---
+++ src/test/Page.jsx
@@ -0,0 +1,8 @@
+function Page() {
+    return (
+        <>
+        jkhjhfdg
+        </>
+    )
+}
+export default Page;
